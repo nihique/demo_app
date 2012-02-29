6e4941eccab7fe73e0275d325c78c9db3e482d47
Martin Kovarik <kovarik.martin@gmail.com>
2012-02-29 19:32:05 +0100

    initial commit

---
+++ demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'a5d985fa0b219856fd0788572e1c2e3fd60edb0e1b3bf7d40ddf0ca18d72514e8ed76daf26da5dab01e724fc60099e000136f0b970b9c96d17f8b80511e863b2'
---
+++ ruby_mine_project/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyMineProject::Application.config.secret_token = '10840ecc0e412608e0735b0c7f03a198d784187033b2f20839960b4e49dd6c7817a02aa8a693789a38f1b3c60779d6571109c3c4e9ded08b81e2dd061596a3cb'
